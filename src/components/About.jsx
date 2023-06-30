27b7acb6ed112c0bf17c2a7a8a301b31f685d8d0
John <tungusv89@gmail.com>
2023-06-30 09:12:58 +0300

    first app react

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import Services from './Services';
 
 const About = () => {
+  const skills =
+  [
+    { name: 'UI & UXDESIGNING', image:'/src/assets/icons/ruler-pen.png'},
+    { name: 'WEB DEVELOPMENT', image:'/src/assets/icons/code.png'},
+    { name: 'MOBILE DEVELOPMENT', image:'/src/assets/icons/android.png'},
+    { name: 'WEB SCRAPING WHITH PYTHON', image:'/src/assets/icons/web-scraping.png'}
+  ]
   return (
-    <div>
-      About
+    <div className='px-7 md:px-10 sm:-mt-24' id="about">
+      <h1 className='text-3xl mt-5 text-primary font-semibold'>About me</h1>
+      <p className='text-white my-2 md:w-2/3 leading-[2]'>Hi, my name is Jayjay Dinero Dinero, i am a Fullstack web developer, UI designer, and Mobile developer. I jhave honed my skills in Web Development and advance i have core understanding of advance UI design principles.
+Here are the major skiills i have. </p>
+    {/* expeiences section */}
+      <div className='md:flex items-center my-7'>
+        <p className='text-primary text-8xl font-bold'>5+</p>
+        <p className='text-white text-2xl md:ml-5'>Years of experience. Specialised in building apps, while ensuring a seamless
+  web experience for end users.</p>
+      </div>
+    {/* skills card */}
+      <div className='flex flex-col md:flex-row'>
+        {
+          skills.map((skill, index) => <div key={index} className='skills md:w-[256px] bg-light hover:bg-primary flex flex-col items-baseline justify-end my-3 md:m-3 p-5 shadow-sm transition-all duration-500'>
+            <img src={skill.image} alt="" />
+            <p className='text-2xl  mt-3 text-white font-semibold'>{skill.name}</p>
+          </div>)
+        }
+      </div>
+
+      {/* services card */}
+      <Services/>
     </div>
   );
 };
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import facebook from '../assets/socials/facebook.png';
+import instagram from '../assets/socials/instagram.png';
+import dribbble from '../assets/socials/dribbble.png';
+import email from '../assets/socials/sms.png';
+
 
 const Contact = () => {
   return (
-    <div>
-      Contact
+    <div className='px-7 md:px-10 my-10' id='contact'>
+      <div className='flex flex-col md:flex-row'>
+        {/* text and icons */}
+        <div className='md:w-1/2'>
+          <h1 className='text-3xl mt-5 text-primary font-semibold'>Connect with me:</h1>
+          <p className='text-white my-2 md:w-2/3 leading-[2]'>I have worked on many projects over the course of being a Web Developer, here are a few of my live, real-world projects</p>
+
+          {/* social icons */}
+          <div className='flex'>
+            <a href=""><img src={facebook} alt="" /></a>
+            <a href="" className='ml-4'><img src={instagram} alt="" /></a>
+            <a href="" className='ml-4'><img src={dribbble} alt="" /></a>
+            <a href="" className='ml-4'><img src={email} alt="" /></a>
+          </div>
+        </div>
+
+        {/* form */}
+        <div className='md:w-1/2'>
+          <p className='mt-16 text-white text-2xl mb-6'>Contact me, let's make magic together</p>
+
+          <form>
+            <input type="text" name='name' id='name' placeholder='Name: ' className='bg-[#f5f5f5] bg-opacity-10 p-3 md:w-3/4 w-full rounded' />
+            <input type="email" name='email' id='email' placeholder='Your Email: ' className='bg-[#f5f5f5] bg-opacity-10 p-3 md:w-3/4 w-full rounded my-3' />
+            <textarea name="message" id="message" placeholder='Message: ' className='bg-[#f5f5f5] bg-opacity-10 p-3 md:w-3/4 w-full rounded resize-y my-3'></textarea>
+            <button type="submit" className='btn bg-primary py-2 px-12 text-white hover:bg:white hover:text-primary block transition-all duration-500'>Send</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
